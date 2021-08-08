ee3bdaba3a2565a343cc5fec36cd32e8535a6f0c
Pavel <pavelvaloshyn89@gmail.com>
2021-08-08 02:31:28 +0200

    fix: fix bags

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable no-console */
-import React/* , { useState} */ from 'react';
+import React, {useState}/* , { useState} */ from 'react';
 import s from './styles/app.component.css';
 
 export const App = () => {
-  // const [firstName, setFirstName] = React.useState(1);
-  const [firstName, setFirstName] = React.useState(0);
+  const [firstName, setFirstName] = useState('');
 
   const handlerSubmit = (event) =>{
     event.preventDefault();
 
-    console.log('TEST');
+    console.log(firstName);
   }
 
   return (
@@ -21,13 +20,11 @@ export const App = () => {
             <form className="name" onSubmit={handlerSubmit}>
 
                 <label className='item' htmlFor='firstName'>
-
                     name:<input type="text" name='firstName value' value={firstName} onChange={(event) => setFirstName(event.target.value)} />
                     </label>
+
                <div>
-
                    <input className={s.btn} type="submit" value="Send" />
-
                 </div>
 
                    </form>
