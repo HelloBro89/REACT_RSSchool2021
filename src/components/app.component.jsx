a829bf7cbeadb748269cac6cce466111504d96bb
Pavel <pavelvaloshyn89@gmail.com>
2021-08-08 19:30:32 +0200

    feat: added all css and stucture of html

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable no-console */
-import React, {useState}/* , { useState} */ from 'react';
+import React, {useState} from 'react';
 import s from './styles/app.component.css';
+import swt from './styles/switcherStyle.css'
 
 export const App = () => {
   const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [birthDate, setBirthDate] = useState();
+  const [country, setCountry] = useState('');
 
   const handlerSubmit = (event) =>{
     event.preventDefault();
@@ -12,24 +17,65 @@ export const App = () => {
   }
 
   return (
+            <form  className={s.form} onSubmit={handlerSubmit}>
 
-    <div className={s.wrapper}>
- 
-        <div className={s.form}>
+                <label className={s.label} htmlFor='firstName'>
+                   Name:
+                    <div>
+                      <input type="text" name='firstName' value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+                    </div>
+                </label>
 
-            <form className="name" onSubmit={handlerSubmit}>
+                <label className={s.label} htmlFor='lastName'>
+                     LastName:
+                  <div>
+                    <input type="text" name='lastName' value={lastName} onChange={(event) => setLastName(event.target.value)} />
+                  </div> 
+                </label>
 
-                <label className='item' htmlFor='firstName'>
-                    name:<input type="text" name='firstName value' value={firstName} onChange={(event) => setFirstName(event.target.value)} />
-                    </label>
+                <label className={s.label} htmlFor='postCode'>
+                  Post code:
+                  <div>
+                    <input type="text" name="postCode" value={postCode} onChange={(event) => setPostCode(event.target.value)} />
+                  </div> 
+                </label>
 
-               <div>
+                <label className={s.label} htmlFor='birthDate'>
+                  Birth date:
+                  <div>
+                    <input type="date" name='birthDate' value={birthDate} onChange={(event) => setBirthDate(event.target.value)} />
+                  </div> 
+                </label>
+
+                <label className={s.label} htmlFor='country'>
+                  Country:
+                  <div>
+                    <select name="country" id="" value={country} onChange={(event) => setCountry(event.target.value)}>
+                      <option>Сhoose country</option>
+                      <option>Belarus</option>
+                      <option>Russia</option>
+                      <option>Poland</option>
+                     <option>Ukrain</option>
+                    </select>
+                  </div> 
+                </label>
+
+                <label className={s.label} htmlFor='agreement'>
+                    <input name='agreement' type="checkbox"/>
+                    I agree to the processing of personal data
+                </label>
+
+                <div style={{marginTop: "10px"}}>Male:</div>
+                <input type="checkbox" id="switch" name='switch' className={swt.checkbox}/>
+               
+                <label htmlFor="switch" className={swt.toggle}></label>
+               
+
+               <div style={{marginTop: "20px"}}>
                    <input className={s.btn} type="submit" value="Send" />
                 </div>
 
                    </form>
 
-         </div> 
-    </div>
   );
 };
