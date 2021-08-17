a74d6b2c779369565da6b22b27e25c058ce9bba8
Pavel <pavelvaloshyn89@gmail.com>
2021-08-17 03:14:40 +0200

    preFinish: added all function: search, search by page, sorting and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
 import axi from './components/services/api.jsx';
+import {Articles} from './components/Articles.jsx';
 
+const myKey = 'df3b0e4161374d6d9c5de1b83b9d7838';
 
 export const App =  () => {
 
   const [searchVal, setSerchVal] = useState('');
+  const [artcls, setArtcls] = useState([]);
+  const [sortBy, setSortBy] = useState('relevancy');
+  const [page, setPage] = useState(1);
 
-const  sendReq = async (e) => {
- e.preventDefault();
- const test = await axi.get(`v2/everything?q=${searchVal}&apiKey=df3b0e4161374d6d9c5de1b83b9d7838`);
-  console.log(test);
-}
+  const sendReq = async (e) => {
+    e.preventDefault();
+    const res = await axi.get(`v2/everything?q=${searchVal}&apiKey=${myKey}&sortBy=${sortBy}&pageSize=5&page=${page}`);
+    setArtcls(res.data.articles);
+  }
 
   return(
     <div>
-<div style={{marginBottom: '10px', marginLeft: '10%'}}>
-        <input style={{height: '50px'}} type="search" value={searchVal} placeholder="Search here..." name="search" onChange={(e) => setSerchVal(e.target.value)} />
-        <button style={{height: '50px',width: '60px' , paddingBottom: '1px'}}  type="submit" onClick={(e) => {sendReq(e)}}>Search</button>
+
+      <form action="" onSubmit={(e) => {sendReq(e)}}>
+      <div style={{marginBottom: '10px', marginLeft: '10%'}}>
+        <input style={{height: '50px'}} type="search" value={searchVal} autoComplete="off" placeholder="Search here..." name="search" onChange={(e) => setSerchVal(e.target.value)} />
+        <button style={{height: '50px',width: '60px' , paddingBottom: '1px'}}  type="submit">Search</button>
+
+        <label  htmlFor='sorting'>
+                    <select name="sorting" id="sorting" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                      <option>relevancy</option>
+                      <option>popularity</option>
+                      <option>publishedAt</option>
+                    </select>
+         </label>
+       </div>
+      </form>
+    <div> 
+      <Articles articles={artcls} page={page} onChangePage={(pageFromInput) => setPage(pageFromInput)} />
     </div>
-</div>
+
+  </div>
   )
 }
 
---
+++ src/components/Articles.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import s from './styles/styles.css';
+
+export const Articles = ({articles, page, onChangePage}) => {
+    
+    const [aPage, setAPage] = useState('');
+
+    useEffect(() => {
+        setAPage(page);
+      }, [page]);
+
+    
+    const handleChange = (e) => {
+        const { value } = e.target;
+        const regexp = /\d+/;
+        const matchedValue = value.match(regexp);
+        if (matchedValue) {
+          const newValue = +matchedValue[0];
+          onChangePage(newValue);
+          setAPage(newValue);
+        } else {
+            setAPage('');
+        }
+      };
+
+    return(
+    <div>
+        {articles.length ? <table className={s.table} >
+            <tbody>
+            <tr className={s.headerOfTable}> 
+                <td >Author</td>
+                <td>Tittle</td>
+                <td>Description</td>
+                <td>Published At</td>
+                <td>Content</td>
+                <td>Image</td>
+            </tr>
+            
+        
+        {
+            articles.map((item, ind)  => (
+                    <tr  key={ind}>
+                        <td className={s.firstTD}>{item.author}</td>
+                        <td>{item.title}</td>
+                        <td>{item.description}</td>
+                        <td>{item.publishedAt}</td>
+                        <td>{item.content}</td>
+                        <td>
+                            <img src={item.urlToImage} width={200} alt={item.title} />
+                        </td>
+                    </tr>
+                ))
+    }
+        </tbody>
+    </table> : null}
+<label htmlFor="getPage">
+    <input name='getPage' type='text' value={aPage} onChange={e => {handleChange(e)}} />
+</label>
+    
+</div>
+    )
+    }
