31e63f7ecdf2e9a59f6fdd2752160e43fe0d534b
Pavel <pavelvaloshyn89@gmail.com>
2021-11-25 23:22:23 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export const App = () => (
         <div style={{backgroundColor: 'rgba(128, 128, 128, 0.250)', minHeight: '90vh', borderRadius: '10px' }}>
             <Header />
         </div>
-        <div style={{backgroundColor: 'grey', width: 'auto', height: '8vh'}}>
-            <div style={{display: 'flex',  justifyContent: 'center', paddingTop: '2%'}}>
+        <div style={{backgroundColor: 'grey', width: 'auto', height: '8vh', paddingTop: '1%'}}>
+            <div style={{display: 'flex',  justifyContent: 'center', }}>
                 <a href="https://github.com/HelloBro89/news-site-react-redux-API">Link to the app in github</a>
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
