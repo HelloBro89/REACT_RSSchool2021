04e0afa161dd2ec0902f1c0bed67c99341ac8c86
Pavel <pavelvaloshyn89@gmail.com>
2021-11-25 23:54:29 +0100

    refactor: added some style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ export const About = () => (
         <br />&nbsp;&nbsp;&nbsp;&nbsp;To view the installation of the application, follow the link to GitHub:
             <br /><a style={{fontSize: '70%'}} href="https://github.com/HelloBro89/news-site-react-redux-API">https://github.com/HelloBro89/news-site-react-redux-API</a>
         <p style={{marginBottom: '0px'}}>&nbsp;&nbsp;&nbsp;&nbsp;This application is used for keyword searches. In the SEARCH field, enter a word or several keywords and click SEARCH. 
-            You can also sort the found material by relevance, popularity and publication date.
-         &nbsp;&nbsp;&nbsp;&nbsp;Below you can enter the page number of the result in the search output.</p>
+            You can also sort the found material by relevance, popularity and publication date.Below you can enter the page number of the result in the search output.</p>
     </div>
 )
