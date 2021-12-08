e9f35b1c0cbd31988001788a346f939f24ce72a1
Pavel <pavelvaloshyn89@gmail.com>
2021-12-08 21:30:15 +0100

    fix: refresh page bag

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,129 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+let pathToCss = '/';
 let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+  pathToCss = '';
 }
+
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode,
-  entry: './src/index.jsx',
-  devtool: 'source-map',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    // filename: 'bundle.js'
+  entry: {
+    main: './index.jsx',
+    // testEntry: "./testEntry.js",
   },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: pathToCss,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
     port: 3000,
     open: true,
   },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
   module: {
     rules: [
       {
-        test: /\.jsx/,
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[name].[ext]',
+              outputPath: './assets',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(tsx|jsx|js)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-    new CleanWebpackPlugin(),
-  ],
 };
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// let mode = 'development';
+// if (process.env.NODE_ENV === 'production') {
+//   mode = 'production';
+// }
+// module.exports = {
+//   mode,
+//   entry: './src/index.jsx',
+//   devtool: 'source-map',
+//   output: {
+//     filename: '[name].[contenthash].js',
+//     path: path.resolve(__dirname, 'dist'),
+//     // filename: 'bundle.js'
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     port: 3000,
+//     open: true,
+//   },
+//   performance: {
+//     hints: false,
+//     maxEntrypointSize: 512000,
+//     maxAssetSize: 512000,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: [/* MiniCssExtractPlugin.loader, */ 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//     // new MiniCssExtractPlugin({
+//     //   filename: 'styles.css',
+//     // }),
+//     new CleanWebpackPlugin(),
+//   ],
+// };
