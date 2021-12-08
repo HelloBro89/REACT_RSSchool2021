1bc429521aae2ef9e0831ccfb4fad98a0e485b36
Pavel <pavelvaloshyn89@gmail.com>
2021-12-08 19:01:10 +0100

    ref: changed key

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Articles } from './Articles.jsx';
-import { addSearchValue, changeSorting, setPage, noChange } from './redux/actions/actionsDashboard.jsx';
+import {
+  addSearchValue,
+  changeSorting,
+  setPage,
+  noChange,
+} from './redux/actions/actionsDashboard.jsx';
 import { getDataNews } from './redux/actions/actionArticles.jsx';
 
-const myKey = 'df3b0e4161374d6d9c5de1b83b9d7838';
+const myKey = 'd4f82bedc4e94e218817ff78386406bc';
 
 export const Dashboard = () => {
-
   const searchFilters = useSelector((state) => state.dashboard);
   const dispatch = useDispatch();
 
@@ -26,20 +30,45 @@ export const Dashboard = () => {
       const newValue = +matchedValue[0];
       dispatch(setPage(newValue));
     } else {
-      dispatch(noChange(""));
+      dispatch(noChange(''));
     }
   };
 
   return (
-
     <div style={{ marginTop: '40px' }}>
-      <form action="" onSubmit={(e) => { sendReq(e) }}>
+      <form
+        action=""
+        onSubmit={(e) => {
+          sendReq(e);
+        }}
+      >
         <div style={{ marginBottom: '10px', marginLeft: '10%' }}>
-          <input style={{ height: '50px' }} type="search" value={searchFilters.value} autoComplete="off" placeholder="Search here..." name="search" onChange={(e) => dispatch(addSearchValue(e.target.value))} /* onChange={(e) => setSerchVal(e.target.value)} */ />
-          <button style={{ height: '50px', width: '60px', paddingBottom: '1px' }} type="submit">Search</button>
+          <input
+            style={{ height: '50px' }}
+            type="search"
+            value={searchFilters.value}
+            autoComplete="off"
+            placeholder="Search here..."
+            name="search"
+            onChange={(e) =>
+              dispatch(addSearchValue(e.target.value))
+            } /* onChange={(e) => setSerchVal(e.target.value)} */
+          />
+          <button
+            style={{ height: '50px', width: '60px', paddingBottom: '1px' }}
+            type="submit"
+          >
+            Search
+          </button>
 
-          <label htmlFor='sorting'>
-            <select name="sorting" value={searchFilters.sorting} onChange={(e) => dispatch(changeSorting(e.target.value))} /* onChange={(e) => setSortBy(e.target.value)} */>
+          <label htmlFor="sorting">
+            <select
+              name="sorting"
+              value={searchFilters.sorting}
+              onChange={(e) =>
+                dispatch(changeSorting(e.target.value))
+              } /* onChange={(e) => setSortBy(e.target.value)} */
+            >
               <option>relevancy</option>
               <option>popularity</option>
               <option>publishedAt</option>
@@ -48,18 +77,21 @@ export const Dashboard = () => {
         </div>
       </form>
 
-      <div >
+      <div>
         <Articles />
       </div>
 
       <div style={{ margin: '50px 0px 0px 80px' }}>
         <label htmlFor="getPage">
-          <input style={{ width: '50px', borderColor: 'red' }} name='getPage' type='text' value={searchFilters.page} onChange={e => handleChangePage(e)} />
+          <input
+            style={{ width: '50px', borderColor: 'red' }}
+            name="getPage"
+            type="text"
+            value={searchFilters.page}
+            onChange={(e) => handleChangePage(e)}
+          />
         </label>
       </div>
-
     </div>
-  )
-}
-
-
+  );
+};
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDataNews } from './redux/actions/actionArticles.jsx';
-import './styles/styles.css'
+import './styles/styles.css';
 
-const myKey = 'df3b0e4161374d6d9c5de1b83b9d7838';
+const myKey = 'd4f82bedc4e94e218817ff78386406bc';
 
 export const Details = () => {
-    const response = useSelector((state) => state.articles.data);
-    const dispatch = useDispatch();
+  const response = useSelector((state) => state.articles.data);
+  const dispatch = useDispatch();
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
+  useEffect(() => {
+    if (!response.length) {
+      const URL = `v2/everything?qInTitle=${id}&apiKey=${myKey}`;
+      dispatch(getDataNews(URL));
+    }
+  }, []);
 
-        if (!response.length) {
-            const URL = `v2/everything?qInTitle=${id}&apiKey=${myKey}`;
-            dispatch(getDataNews(URL));
-        }
-    }, []);
-
-    return (<div>
-        {
-            response.filter(elem => elem.title === id).slice(0, 1).map((item, ind) => (
-                <div key={ind} style={{ fontSize: "20px" }}>
-                    <div> <span>Author:</span>  {item.author}</div>
-                    <div> <span>Tittle:</span>  {item.title}</div>
-                    <div> <span>Description:</span>  {item.description}</div>
-                    <div> <span>Published At:</span>  {item.publishedAt}</div>
-                    <div> <span>Content:</span>  {item.content}</div>
-                    <div> <span>Image:</span>  <img src={item.urlToImage} width={200} alt={item.title} /></div>
-                </div>)
-            )
-        }
+  return (
+    <div>
+      {response
+        .filter((elem) => elem.title === id)
+        .slice(0, 1)
+        .map((item, ind) => (
+          <div key={ind} style={{ fontSize: '20px' }}>
+            <div>
+              {' '}
+              <span>Author:</span> {item.author}
+            </div>
+            <div>
+              {' '}
+              <span>Tittle:</span> {item.title}
+            </div>
+            <div>
+              {' '}
+              <span>Description:</span> {item.description}
+            </div>
+            <div>
+              {' '}
+              <span>Published At:</span> {item.publishedAt}
+            </div>
+            <div>
+              {' '}
+              <span>Content:</span> {item.content}
+            </div>
+            <div>
+              {' '}
+              <span>Image:</span>{' '}
+              <img src={item.urlToImage} width={200} alt={item.title} />
+            </div>
+          </div>
+        ))}
     </div>
-    )
-}
+  );
+};
